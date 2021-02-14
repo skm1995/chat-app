a34baaf0b8eba0e04d85bb4eb6a9cd91ad5f2b87
Szymon Kozłowski <szymonkozlowski1995@gmail.com>
2021-02-14 13:00:25 +0100

    user joined / left message

--- src/index.js
+++ src/index.js
@@ -15,11 +15,16 @@ app.use(express.static(publicDirectoryPath));
 io.on('connection', (socket) => {
     console.log('New WebSocket connection.');
 
-    socket.emit('message', 'Welcome!')
+    socket.emit('message', 'Welcome!');
+    socket.broadcast.emit('message', 'A new user has joined!');
 
     socket.on('sendMessage', (message) => {
         console.log(message);
         io.emit('message', message);
+    });
+
+    socket.on('disconnect', () => {
+        io.emit('message', 'A user has left!');
     });
 });
 
